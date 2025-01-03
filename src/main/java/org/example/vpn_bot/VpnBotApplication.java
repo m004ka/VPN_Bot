44ebdec7d2ea75ba16dd0631363f36168ca195c8
m004ka <ysupov959@gmail.com>
2025-01-03 18:46:35 +0300

    first

---
+++ src/main/java/org/example/vpn_bot/VpnBotApplication.java
@@ -0,0 +1,13 @@
+package org.example.vpn_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VpnBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VpnBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/vpn_bot/VpnBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.vpn_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VpnBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
