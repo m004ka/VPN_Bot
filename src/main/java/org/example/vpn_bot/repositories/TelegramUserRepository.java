3428961556e3227b7c728e119a2a4c198af265a7
m004ka <ysupov959@gmail.com>
2025-01-04 10:45:18 +0300

    work

--- src/main/java/org/example/vpn_bot/repositories/TelegramUserRepository.java
+++ src/main/java/org/example/vpn_bot/repositories/TelegramUserRepository.java
@@ -4,7 +4,11 @@ import org.example.vpn_bot.models.TelegramUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TelegramUserRepository extends JpaRepository<TelegramUser, Long> {
     Boolean existsTelegramUserByChatId(Long id);
+
+    Optional<TelegramUser> findByChatId(Long id);
 }
