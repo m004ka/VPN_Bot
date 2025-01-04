8ea2453ae5800082ee604b65b93f35bf45fcf932
m004ka <ysupov959@gmail.com>
2025-01-04 03:12:42 +0300

    Репозиторий юзера

---
+++ src/main/java/org/example/vpn_bot/repositories/TelegramUserRepository.java
@@ -0,0 +1,9 @@
+package org.example.vpn_bot.repositories;
+
+import org.example.vpn_bot.models.TelegramUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelegramUserRepository extends JpaRepository<TelegramUser, Long> {
+}
