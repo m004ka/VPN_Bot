20ffcc1b618b1f4179483f6cc2693fe98dbf8106
m004ka <ysupov959@gmail.com>
2025-01-04 05:02:20 +0300

    Исправление метода

--- src/main/java/org/example/vpn_bot/repositories/TelegramUserRepository.java
+++ src/main/java/org/example/vpn_bot/repositories/TelegramUserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TelegramUserRepository extends JpaRepository<TelegramUser, Long> {
+    Boolean existsTelegramUserByChatId(Long id);
 }
