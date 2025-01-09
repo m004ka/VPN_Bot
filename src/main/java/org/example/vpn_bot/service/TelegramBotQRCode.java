554ef20b396ee929689df8e99e2c36720ec549f4
m004ka <ysupov959@gmail.com>
2025-01-09 06:30:21 +0300

    класс куаркода

---
+++ src/main/java/org/example/vpn_bot/service/TelegramBotQRCode.java
@@ -0,0 +1,28 @@
+package org.example.vpn_bot.service;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+@Service
+@RequiredArgsConstructor
+public class TelegramBotQRCode {
+
+    // Метод для генерации QR-кода и возврата его как ByteArrayInputStream
+    public ByteArrayInputStream generateQRCodeStream(String text, int width, int height) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream);
+
+        return new ByteArrayInputStream(outputStream.toByteArray());
+    }
+}
