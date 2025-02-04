64994187fa53c00bcc9ed4b24e12e800c2651902
m004ka <ysupov959@gmail.com>
2025-02-04 04:37:46 +0300

    EnableScheduling

--- src/main/java/org/example/vpn_bot/VpnBotApplication.java
+++ src/main/java/org/example/vpn_bot/VpnBotApplication.java
@@ -2,8 +2,10 @@ package org.example.vpn_bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class VpnBotApplication {
 
     public static void main(String[] args) {
